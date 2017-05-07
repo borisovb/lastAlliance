8a8ff26046df4bc6b5b25004e8d9d75916237f03
BBorisov <puple.bsb@gmail.com>
2017-05-07 17:10:47 +0300

    fixed episode number view

--- resources/views/episodes/watch.blade.php
+++ resources/views/episodes/watch.blade.php
@@ -3,11 +3,9 @@
 @section('content')
     <div class="row">
         <div class="col-md-10">
-            <h2 class="text-center red">{{ $episode->name_bg }}</h2>
             <h4><a class="episode-title" href="{{ route('view_project', $episode->project->slug) }}">{{ $episode->project->title }}</a></h4>
-            <hr>
             <div style="margin: 9px" class="row">
-            <span style="font-size: 20px"><strong> Епизод:</strong> {{ $episode->number }}</span>
+            <span style="font-size: 20px"><strong> Епизод:</strong> [ {{ $episode->number < 10 ? '0' : '' }}{{ $episode->number }} ] - {{ $episode->name_bg }}</span>
             @if($next)
                 <a  style="margin-left: 10px;" href="#" class="btn btn-danger btn-pressure pull-right">Следващ</a>
             @endif
