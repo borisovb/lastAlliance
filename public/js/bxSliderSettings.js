6d19cea3ffa750cac45b27c74998e3d53c65f430
NikolaNikolov <whitestarhs@gmail.com>
2017-10-15 14:27:41 +0300

    fixed slider again

--- public/js/bxSliderSettings.js
+++ public/js/bxSliderSettings.js
@@ -3,15 +3,15 @@ $(document).ready(function(){
     $('.slider1').bxSlider({
         slideWidth: 230,
         onSliderLoad: function(){
-            $(".slider1").css("visibility", "visible");
+            let sliderDiv = document.querySelector('.slider1');
+            sliderDiv.style.visibility = "visible";
         },
-        autoStart: true,
         keyboardEnabled: true,
         autoDelay: 0,
         slideHeight: 230,
         minSlides: 2,
         maxSlides: 20,
         slideMargin: 5,
-        pager: true
+        pager: false
     });
 });
